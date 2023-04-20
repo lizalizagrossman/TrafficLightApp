a0a9347f282d337ccdd8e068c6c0f2011d97005b
Liza Grossman <lisok.sam@gmail.com>
2023-04-20 18:22:18 +0300

    Before button config

--- TrafficLightApp/ContentView.swift
+++ TrafficLightApp/ContentView.swift
@@ -8,14 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    private enum CurrentLight {
+        case red, yellow, green
+    }
+    
+    @State private var currentLight = CurrentLight.red
+    @State private var greenOpacity = 0.3
+    @State private var yellowOpacity = 0.3
+    @State private var redOpacity = 0.3
+    @State private var firstTimeTap = true
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Circle()
+                .frame(width: 130)
+                .foregroundColor(.red)
+                .opacity(redOpacity)
+                .overlay(Circle().stroke(Color.white, lineWidth: 3))
+            Circle()
+                .frame(width: 130)
+                .foregroundColor(.yellow)
+                .opacity(yellowOpacity)
+                .overlay(Circle().stroke(Color.white, lineWidth: 3))
+            Circle()
+                    .frame(width: 130)
+                    .foregroundColor(.green)
+                    .opacity(greenOpacity)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 3))
+            Spacer()
+            Button(action: changeColor ) {
+                Text(firstTimeTap ? "Start" : "Next")
+                    .font(.title)
+                    .foregroundColor(.black)
+            }
+                .buttonStyle(.borderedProminent)
+                .tint(.white)
+                .controlSize(.large)
         }
-        .padding()
+    }
+    
+    func changeColor() {
+        firstTimeTap = false
+        switch currentLight {
+        case .red:
+            greenOpacity = 0.3
+            redOpacity = 1.0
+            currentLight = .yellow
+        case .yellow:
+            redOpacity = 0.3
+            yellowOpacity = 1.0
+            currentLight = .green
+        case .green:
+            yellowOpacity = 0.3
+            greenOpacity = 1.0
+            currentLight = .red
+        }
     }
 }
 
