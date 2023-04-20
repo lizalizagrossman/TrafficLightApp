8b95d823b47bd070b8123ca7c86db8cec75b5923
Liza Grossman <lisok.sam@gmail.com>
2023-04-20 16:36:16 +0300

    Initial Commit

---
+++ TrafficLightApp/TrafficLightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightAppApp.swift
+//  TrafficLightApp
+//
+//  Created by Elizabeth on 20/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
